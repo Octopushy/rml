e2b7ee642dcc7baafa84fb2bd8969065d36d62a3
Nicholas Williams <ntwilliams.personal@gmail.com>
2019-03-22 10:02:21 -0400

    working prelim simulation models

--- code/rml_models.R
+++ code/rml_models.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(lme4)
 library(broom)
 library(emmeans)
+library(modelr)
 
 # a little housekeeping ---------------------------------------------------
 
@@ -21,4 +22,44 @@ b_emm <- emmeans(b, "rml", by = "age_grp")
 pairs(b_emm) %>% 
   tidy() %>% 
   filter(level2 == "before")
+
+# mapping over simulations ------------------------------------------------
+
+rml_map <- simulate %>% 
+  mutate(models = map(data, ~lmer(prev ~ year + age_grp + rml + age_grp*year + age_grp*rml + (1 | state), data = .)))
+
+rml_map <- rml_map %>% 
+  mutate(emm = map(models, ~emmeans(., "rml", "age_grp")))
+
+rml_map <- rml_map %>% 
+  mutate(pair = map(emm, pairs), 
+         pair = map(pair, tidy), 
+         pair = map(pair, ~filter(., level2 == "before")))
+
+# results -----------------------------------------------------------------
+
+rml_map %>% 
+  unnest(pair) %>% 
+  filter(age_grp == "12_17") %>% 
+  ggplot(aes(x = estimate)) + 
+  geom_density()
+
+rml_map %>% 
+  unnest(pair) %>% 
+  group_by(age_grp) %>% 
+  dplyr::summarize(avg = mean(estimate), 
+                   lower = quantile(estimate, 0.025), 
+                   upper = quantile(estimate, 0.975))
+
+rml_map %>% 
+  unnest(pair) %>% 
+  filter(age_grp == "18_25") %>% 
+  ggplot(aes(x = estimate)) + 
+  geom_density()
+
+rml_map %>% 
+  unnest(pair) %>% 
+  filter(age_grp == "26_plus") %>% 
+  ggplot(aes(x = estimate)) + 
+  geom_density()
 
