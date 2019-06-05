86ca193c6e7d03812efebfbcf5927338bb140f3b
Nicholas Williams <ntwilliams.personal@gmail.com>
2019-06-05 13:51:06 -0400

    work around emmeans bottleneck

---
+++ code/rml_models_alc.R
@@ -0,0 +1,140 @@
+library(tidyverse)
+library(lme4)
+library(broom)
+library(furrr)
+library(emmeans)
+library(modelr)
+library(jsonlite)
+
+# a little housekeeping ---------------------------------------------------
+
+source("code/simulation.R")
+rml <- readRDS("data/clean/rml_pm_alc_07_17.rds")
+
+rml_mod <- rml %>% 
+  select(-c(se_12_17:se_26_plus)) %>% 
+  gather(key = age_grp, value = prev, `12_17`:`26_plus`)
+
+# base model --------------------------------------------------------------
+
+b <- lmer(prev ~ year + age_grp + rml + age_grp*year + age_grp*rml + (1 | state), data = rml_mod)
+
+b_t_3 <- anova(b) %>% tidy()
+
+b_est <- b %>% tidy()
+
+b_emm <- emmeans(b, "rml", by = "age_grp")
+
+b_pairs <- pairs(b_emm) %>% 
+  tidy() %>% 
+  filter(level2 == "before")
+
+base_model <- list(
+  aov = b_t_3,
+  estimates = b_est,
+  emm = b_emm %>% tidy(),
+  pairs = b_pairs
+)
+
+toJSON(base_model, pretty = TRUE) %>%
+  write_lines("data/base_model_alc.json")
+
+# simulation --------------------------------------------------------------
+
+simulate <- simulate(rml)
+
+saveRDS(simulate, "data/clean/rml_simulation_alc.rds")
+
+# mapping over simulations ------------------------------------------------
+
+rml_map <- simulate %>% 
+  mutate(models = map(data, ~lmer(prev ~ year + age_grp + rml + age_grp*year + age_grp*rml + (1 | state), data = .)))
+
+rml_map <- rml_map %>% 
+  mutate(emm = map(models, ~emmeans(., "rml", "age_grp"))) 
+
+rml_map <- rml_map %>% 
+  mutate(pair = map(emm, pairs))
+
+# test <- test %>% 
+#   mutate(pair = map(pair, function(.data) .data[c(1, 4, 7)]))
+
+make_df <- function(sim, data, models, emm, pair, ...) {
+  x <- tibble(
+    state = rep("Alabama", 6),
+    year = rep("2007", 6), 
+    rml = c(rep("after", 3), rep("before", 3)), 
+    age_grp = rep(c("12_17", "18_25", "26_plus"), 2)
+  )
+  return(x)
+}
+
+make_pairs <- function(.data) {
+  .data %>% 
+    select(-c(state, year)) %>% 
+    group_by(rml, age_grp) %>% 
+    spread(rml, pred) %>% 
+    mutate(pair = after - before)
+}
+
+# test <- slice(rml_map, 1:10)
+# 
+# test <- test %>% 
+#   mutate(pred = pmap(., make_df))
+# 
+# test <- test %>% 
+#   mutate(pred = map2(pred, models, ~add_predictions(.x, .y)))
+# 
+# test <- test %>% 
+#   mutate(pred = map(pred, make_pairs))
+
+rml_map <- rml_map %>% 
+  mutate(pred = pmap(., make_df))
+
+rml_map <- rml_map %>% 
+  mutate(pred = map2(pred, models, ~add_predictions(.x, .y)))
+
+rml_map <- rml_map %>% 
+  mutate(pred = map(pred, make_pairs))
+
+# rml_map <- rml_map %>% 
+#   mutate(pair = map(pair, function(.data) .data[c(1, 4, 7)]))
+# 
+# rml_map_1 <- slice(rml_map, 1:2500)
+# 
+# rml_map_1 <- rml_map_1 %>% 
+#   mutate(pair = map(pair, as_tibble))
+
+# rml_map <- rml_map %>%
+#   mutate(pair = map(pair, ~filter(., level2 == "before")))
+
+# results -----------------------------------------------------------------
+
+rml_map %>% 
+  unnest(pred) %>% 
+  filter(age_grp == "12_17") %>% 
+  ggplot(aes(x = pair)) + 
+  geom_density()
+
+rml_map %>% 
+  unnest(pred) %>% 
+  filter(age_grp == "18_25") %>% 
+  ggplot(aes(x = pair)) + 
+  geom_density()
+
+rml_map %>% 
+  unnest(pred) %>% 
+  filter(age_grp == "26_plus") %>% 
+  ggplot(aes(x = pair)) + 
+  geom_density()
+
+rml_map %>% 
+  unnest(pred) %>% 
+  group_by(age_grp) %>% 
+  dplyr::summarize(avg = mean(pair), 
+                   lower = quantile(pair, 0.025), 
+                   upper = quantile(pair, 0.975))
+
+# saving results ----------------------------------------------------------
+
+saveRDS(rml_map, "data/sim_ten_k_alc_map.rds")
